fb99e8ee01bfb87d535fc1ec0c8039e6091cea4b
evgeniyshylko <y.shylko@gmail.com>
2019-03-22 08:23:41 +0200

    junits

--- game-of-three-player-one/src/test/java/com/takeaway/gameofthree/player/test/PlayerControllerTest.java
+++ game-of-three-player-one/src/test/java/com/takeaway/gameofthree/player/test/PlayerControllerTest.java
@@ -1,13 +1,48 @@
 package com.takeaway.gameofthree.player.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.springframework.jms.core.JmsTemplate;
+import static org.mockito.Mockito.verify; 
+
+import com.takeaway.gameofthree.common.component.Calculator;
+import com.takeaway.gameofthree.common.dto.GameMessage;
+import com.takeaway.gameofthree.playerone.component.PlayerOneMessageHandler;
 
 public class PlayerControllerTest {
 
+	@InjectMocks
+	PlayerOneMessageHandler handler;
+
+	@Mock
+	JmsTemplate JmsTemplate;
+	
+	@Spy
+	Calculator calculator;
+	
+	@Before
+    public void initMocks(){
+        MockitoAnnotations.initMocks(this);
+    }
+
 	@Test
-	public void testGenerateNumber() {
+	public void testCorrectNuberReceivedFromQueue() {
+		GameMessage message = new GameMessage();
+		message.setGameId(4);
+		message.setNumber((long) 100);
+		try {
+			handler.receiveMessage(message);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertTrue(message.getNumber() == 33);
 	}
 
 }
